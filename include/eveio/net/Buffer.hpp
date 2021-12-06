5b107a377bf45b0a19eb119677b6f3d33d477523
LiLonghao <qwqllh@outlook.com>
2021-12-06 22:12:52 +0800

    add Buffer

---
+++ include/eveio/net/Buffer.hpp
@@ -0,0 +1,64 @@
+#ifndef EVEIO_NET_BUFFER_HPP
+#define EVEIO_NET_BUFFER_HPP
+
+#include "eveio/String.hpp"
+#include "eveio/Vector.hpp"
+#include "eveio/net/Socket.hpp"
+
+#include <memory>
+
+namespace eveio {
+namespace net {
+
+class Buffer {
+  Vector<char> storage;
+  size_t head, tail;
+
+public:
+  Buffer() noexcept : storage(), head(0), tail(0) {}
+
+  Buffer(const Buffer &) noexcept = default;
+  Buffer &operator=(const Buffer &) noexcept = default;
+
+  Buffer(Buffer &&) noexcept = default;
+  Buffer &operator=(Buffer &&) noexcept = default;
+
+  ~Buffer() noexcept = default;
+
+  template <class T>
+  T *Data() noexcept {
+    return reinterpret_cast<T *>(&storage[0] + head);
+  }
+
+  template <class T>
+  const T *Data() const noexcept {
+    return reinterpret_cast<const T *>(&storage[0] + head);
+  }
+
+  void Clear() noexcept {
+    storage.shrink_to_fit();
+    head = tail = 0;
+  }
+
+  size_t Size() const noexcept { return (tail - head); }
+
+  bool IsEmpty() const noexcept { return Size() == 0; }
+
+  size_t Capacity() const noexcept { return (storage.capacity() - tail); }
+
+  void Readout(size_t byte) noexcept {
+    head += byte;
+    if (head >= tail)
+      Clear();
+  }
+
+  void Append(const void *data, size_t byte) noexcept;
+
+  // For internal usage. Do not call outside.
+  int ReadFromSocket(detail::native_socket_type sock) noexcept;
+};
+
+} // namespace net
+} // namespace eveio
+
+#endif // EVEIO_NET_BUFFER_HPP
---
+++ src/Buffer.cpp
@@ -0,0 +1,41 @@
+#include "eveio/net/Buffer.hpp"
+#include "eveio/net/Socket.hpp"
+
+#include <cerrno>
+#include <cstring>
+
+static constexpr const int DefaultBufSize = 65536;
+
+void eveio::net::Buffer::Append(const void *data, size_t byte) noexcept {
+  storage.resize(tail + byte);
+  memcpy(storage.data() + tail, data, byte);
+  tail += byte;
+}
+
+int eveio::net::Buffer::ReadFromSocket(
+    detail::native_socket_type sock) noexcept {
+  char buf[DefaultBufSize]{};
+  int tot_read = 0, byte_read = 0;
+
+  for (;;) {
+    byte_read = detail::socket_read(sock, buf, sizeof(buf));
+    int saved_errno = errno;
+
+    if (byte_read > 0) {
+      Append(buf, byte_read);
+      tot_read += byte_read;
+      if (byte_read < DefaultBufSize)
+        break;
+    } else {
+      if (byte_read < 0) {
+        if (saved_errno == ECONNRESET)
+          tot_read = byte_read;
+        else
+          errno = saved_errno;
+      }
+      break;
+    }
+  }
+
+  return tot_read;
+}
