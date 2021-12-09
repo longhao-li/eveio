b60888f6cac332d0bafdde28c1f250909a39a9c0
LiLonghao <qwqllh@outlook.com>
2021-12-09 14:08:38 +0800

    add UdpServer

--- include/eveio/net/Buffer.hpp
+++ include/eveio/net/Buffer.hpp
@@ -1,10 +1,12 @@
 #ifndef EVEIO_NET_BUFFER_HPP
 #define EVEIO_NET_BUFFER_HPP
 
+#include "eveio/Allocator.hpp"
 #include "eveio/String.hpp"
 #include "eveio/Vector.hpp"
 #include "eveio/net/Socket.hpp"
 
+#include <cstring>
 #include <memory>
 
 namespace eveio {
@@ -62,6 +64,54 @@ public:
   // For internal usage. Do not call outside.
   bool ReadFromSocket(detail::native_socket_type sock, int &tot_read) noexcept;
 };
+
+template <size_t Cap = 4096>
+class BasicFixedBuffer {
+  char p[Cap];
+  size_t size;
+
+public:
+  static constexpr const size_t BufferCapacity = Cap;
+
+  constexpr BasicFixedBuffer() noexcept : size(0) {}
+  constexpr BasicFixedBuffer(const void *data, size_t sz) noexcept
+      : p{}, size(sz) {
+    std::memcpy(p, data, sz > Cap ? Cap : sz);
+  }
+  constexpr BasicFixedBuffer(const BasicFixedBuffer &other) noexcept
+      : size(other.size) {
+    std::memcpy(p, other.p, Cap);
+  }
+  constexpr BasicFixedBuffer &
+  operator=(const BasicFixedBuffer &other) noexcept {
+    p = other.p;
+    size = other.size;
+  }
+  ~BasicFixedBuffer() noexcept = default;
+
+  constexpr size_t Capacity() const noexcept { return Cap; }
+
+  size_t Size() const noexcept { return size; }
+  void SetSize(size_t sz) noexcept { size = sz; }
+
+  char *Data() noexcept { return p; }
+  const char *Data() const noexcept { return p; }
+
+  template <class T>
+  T *Data() noexcept {
+    return reinterpret_cast<T *>(p);
+  }
+
+  template <class T>
+  const T *Data() const noexcept {
+    return reinterpret_cast<const T *>(p);
+  }
+
+  constexpr char operator[](size_t i) const noexcept { return p[i]; }
+  constexpr char &operator[](size_t i) noexcept { return p[i]; }
+};
+
+typedef BasicFixedBuffer<4096> FixedBuffer;
 
 } // namespace net
 } // namespace eveio
