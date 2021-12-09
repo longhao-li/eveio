e47349f07cc55fff00bcdf32b01420adce5c4f95
LiLonghao <qwqllh@outlook.com>
2021-12-09 14:46:52 +0800

    add UdpStream

---
+++ include/eveio/net/UdpStream.hpp
@@ -0,0 +1,48 @@
+#ifndef EVEIO_NET_UDPSTREAM_HPP
+#define EVEIO_NET_UDPSTREAM_HPP
+
+#include "eveio/Result.hpp"
+#include "eveio/net/Socket.hpp"
+
+namespace eveio {
+namespace net {
+
+class UdpStream {
+public:
+  typedef detail::native_socket_type native_socket_type;
+  static constexpr const native_socket_type InvalidSocket =
+      detail::InvalidSocket;
+
+private:
+  native_socket_type sock;
+
+  UdpStream(native_socket_type sock) noexcept : sock(sock) {}
+
+public:
+  static Result<UdpStream> Ipv4Stream() noexcept;
+  static Result<UdpStream> Ipv6Stream() noexcept;
+
+  UdpStream(const UdpStream &) = delete;
+  UdpStream &operator=(const UdpStream &) = delete;
+
+  UdpStream(UdpStream &&) noexcept;
+  UdpStream &operator=(UdpStream &&) noexcept;
+
+  ~UdpStream() noexcept;
+
+  int SendTo(StringRef data, const InetAddr &target) const noexcept;
+  int SendTo(const void *data,
+             size_t size,
+             const InetAddr &target) const noexcept;
+  int ReceiveFrom(void *buf, size_t cap, InetAddr &peer) const noexcept;
+
+  bool SetNonblock(bool on) const noexcept;
+
+  native_socket_type native_socket() const noexcept { return sock; }
+  operator native_socket_type() const noexcept { return sock; }
+};
+
+} // namespace net
+} // namespace eveio
+
+#endif // EVEIO_NET_UDPSTREAM_HPP
---
+++ src/UdpStream.cpp
@@ -0,0 +1,63 @@
+#include "eveio/net/UdpStream.hpp"
+#include "eveio/Result.hpp"
+#include "eveio/net/Socket.hpp"
+#include "eveio/net/UdpSocket.hpp"
+
+using namespace eveio;
+using namespace eveio::net;
+
+Result<UdpStream> eveio::net::UdpStream::Ipv4Stream() noexcept {
+  UdpStream::native_socket_type sock = detail::socket(AF_INET, SOCK_DGRAM, 0);
+  if (sock == UdpStream::InvalidSocket)
+    return Result<UdpStream>::Error(std::strerror(errno));
+  return Result<UdpStream>::Ok(UdpStream(sock));
+}
+
+Result<UdpStream> eveio::net::UdpStream::Ipv6Stream() noexcept {
+  UdpStream::native_socket_type sock = detail::socket(AF_INET6, SOCK_DGRAM, 0);
+  if (sock == UdpStream::InvalidSocket)
+    return Result<UdpStream>::Error(std::strerror(errno));
+  return Result<UdpStream>::Ok(UdpStream(sock));
+}
+
+eveio::net::UdpStream::UdpStream(UdpStream &&other) noexcept
+    : sock(other.sock) {
+  other.sock = InvalidSocket;
+}
+
+UdpStream &eveio::net::UdpStream::operator=(UdpStream &&other) noexcept {
+  if (sock != InvalidSocket)
+    detail::close_socket(sock);
+  sock = other.sock;
+  other.sock = InvalidSocket;
+  return (*this);
+}
+
+eveio::net::UdpStream::~UdpStream() noexcept {
+  if (sock != InvalidSocket)
+    detail::close_socket(sock);
+}
+
+int eveio::net::UdpStream::SendTo(StringRef data,
+                                  const InetAddr &target) const noexcept {
+  return SendTo(data.data(), data.size(), target);
+}
+
+int eveio::net::UdpStream::SendTo(const void *data,
+                                  size_t size,
+                                  const InetAddr &target) const noexcept {
+  return detail::socket_sendto(
+      sock, data, size, target.AsSockaddr(), target.Size());
+}
+
+int eveio::net::UdpStream::ReceiveFrom(void *buf,
+                                       size_t cap,
+                                       InetAddr &peer) const noexcept {
+  struct sockaddr *peer_addr = const_cast<struct sockaddr *>(peer.AsSockaddr());
+  socklen_t len = sizeof(peer);
+  return detail::socket_recvfrom(sock, buf, cap, peer_addr, &len);
+}
+
+bool eveio::net::UdpStream::SetNonblock(bool on) const noexcept {
+  return detail::set_nonblock(sock, on);
+}
